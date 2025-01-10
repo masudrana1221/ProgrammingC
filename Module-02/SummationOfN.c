c354772cd6155ed0c72e0e0820e5370da629162e
Masud Rana <masud.mr5622@gmail.com>
2025-01-11 02:32:29 +0800

    add Sumation of N Number.

---
+++ Module-02/SummationOfN.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    int sum = 0;
+    scanf("%d", &a);
+    for (int i = 0; i <= a; i++)
+    {
+        sum = sum + i;
+    }
+    printf("%d", sum);
+    return 0;
+}
