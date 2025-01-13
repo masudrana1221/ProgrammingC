5cec3b477353fb4386df3494444ced0551194e30
Masud Rana <masud.mr5622@gmail.com>
2025-01-13 23:06:05 +0800

    add arethmatic oprator

---
+++ Module-02/arethmatic_opretor.c
@@ -0,0 +1,49 @@
+
+#include<stdio.h>
+#include<math.h>
+
+// floating arithmatic number
+
+int main()
+{
+    float x , y;
+
+    scanf("%f %f", &x, &y);
+
+    float division = x / y;
+
+    printf("Division : %.2f", division);
+
+    return 0;
+}
+
+
+// integer arithmetic number
+
+int main()
+{
+    int num1, num2;
+
+    scanf("%d %d", &num1, &num2);
+
+    int sum = num1 + num2 ;
+
+    printf("Sum of the equation = %d", sum);
+ 
+    return 0;
+}
+
+
+
+// simple arithmaric number
+
+
+int main()
+{
+    int a = 5 , b = 3 ;
+    int sum = a + b;
+
+    printf("%d", sum );
+
+    return 0;
+}
