8263b0a09f8757705f237d7ee9bc706e7d99ec1a
Masud Rana <masud.mr5622@gmail.com>
2025-01-14 23:08:40 +0800

    add increment or decrement operator

---
+++ Module-02/assignment_operator.c
@@ -0,0 +1,25 @@
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a = 15;
+
+    a *= 5;
+
+    /*
+    
+    Could be also these assignment operatros
+    a += 5;
+    a -= 5;
+    a *= 5;
+    a /= 5;
+    a %= 5;
+
+    */
+
+    printf("%d", a);
+
+    return 0;
+}
---
+++ Module-02/in_or_decrement_operator.c
@@ -0,0 +1,22 @@
+
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    int a = 5;
+    printf("Before : %d \n", a++);
+
+    printf("After : %d", a);
+
+}
+
+/*
+
+Increment Operator:
+1) ++i = pre increment operator
+2) i++ = post increment oparetor
+
+1) ++i 
+
+*/
